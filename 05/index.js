bc11f9a733a5544fc4a0935164ec37c53cb5da99
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:27:50 -0300

    Ex05 - Filtrando apenas os pares

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,31 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const original = [1, 4, 12, 21, 2, 53, 88, 31, 112];
+let pares = [];
+
+//Utilizando FOR OF
+
+for (num of original){
+    if (num % 2 == 0){
+        pares.push(num);
+    }
+}
+
+//Utilizando FOR tradicional
+
+/*for (let i = 0; i<=(original.length -1); i++){
+    if (original[i] % 2 == 0){
+        pares.push(original[i]);
+    }
+}*/
+
+//Utilizando WHILE
+
+/*let i = 0;
+while (i <= (original.length - 1)){
+    if (original[i] % 2 == 0){
+        pares.push(original[i]);
+    }
+    i++;
+}*/
+
+console.log(pares);
+
