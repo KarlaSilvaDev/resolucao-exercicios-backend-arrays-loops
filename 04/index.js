7d1b0f8e42568d9590c212e246029dfc1b9d2d02
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:18:51 -0300

    Ex04 - Múltiplos de 3

---
+++ 04/index.js
@@ -0,0 +1,14 @@
+//Utilizando FOR
+
+for (let num = 0; num <=500; num+=3){
+    console.log(num);
+}
+
+//Utilizando WHILE
+
+/*let numero = 0;
+while (numero<=500){
+    console.log(numero);
+    numero += 3;
+}*/
+
