e61e4e796454db2e2a5e4df7525c8ce2173a74a4
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 10:46:30 -0300

    Ex12 - Fila do Banco

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,9 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+const filaDeDentro = ["Jose", "Joao"];
+const filaDeFora = ["Joana", "Roberta"];
 
+while (filaDeDentro.length < 5 && filaDeFora.length > 0){
+    filaDeDentro.push(filaDeFora.shift());
+}
+
+console.log(filaDeDentro);
+console.log(filaDeFora);
