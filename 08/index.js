aa978e02984fa8e331f4ecc18a718cbc2fe7129c
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 10:16:43 -0300

    Ex08 - Encontrando o maior

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,49 @@
-const numeros = [3, 24, 1, 8, 11, 7, 15];
+const numeros = [-3, -24, -1, -8, -11, -7, -15];
+
+
+//Utilizando FOR OF
+
+let cont = 0;
+for (let numero of numeros){
+    if (cont == 0){
+        maior = numero;
+    }else{
+        if (maior < numero){
+            maior = numero;
+        }
+    }
+    cont++;
+}
+
+
+//Utilizando FOR tradicional
+/*
+for (let i = 0; i < numeros.length; i++){
+    if (i == 0){
+        maior = numeros[i];
+    }else{
+        if (maior < numeros[i]){
+            maior = numeros[i];
+        }
+    }
+}
+*/
+
+//Utilizando WHILE
+/*
+let i = 0;
+while (i < numeros.length){
+    if (i == 0){
+        maior = numeros[i];
+    }else{
+        if (maior < numeros[i]){
+            maior =  numeros[i];
+        }
+    }
+    i++;
+}
+*/
+
+
+//RESPOSTA FINAL
+console.log(maior)
