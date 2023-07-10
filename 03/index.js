dfe94da61c5138a32e767579642f906f8737fda0
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:38:59 -0300

    Adicionadas outras estruturas de repetição

--- 03/index.js
+++ 03/index.js
@@ -1,7 +1,8 @@
 const numeros = [54, 22, 14, 87, 10, 284];
+
+//Utilizando FOR OF
 let encontrado = false;
 let indice = 0;
-
 for (let numero of numeros){
     if (numero == 10){
         encontrado = true;
@@ -13,3 +14,34 @@ for (let numero of numeros){
 if (encontrado == false){
     console.log(-1);
 }
+
+//Utilizando o FOR tradicional
+
+/*let encontrado = false;
+
+for (let indice = 0; indice < numeros.length; indice++){
+    if (numeros[indice] == 10){
+        encontrado = true;
+        console.log(indice);
+    }
+}
+
+if (encontrado == false){
+    console.log(-1);
+}*/
+
+//Utilizando WHILE
+
+/*let encontrado = false;
+let indice = 0;
+while (indice < numeros.length){
+    if (numeros[indice] == 10){
+        encontrado = true;
+        console.log(indice);
+    }
+    indice++;
+}
+
+if (encontrado == false){
+    console.log(-1);
+}*/
