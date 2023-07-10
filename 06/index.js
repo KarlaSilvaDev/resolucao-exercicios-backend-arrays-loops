fe12ad4e34fb20bf2a56bf474a545b3cfce74703
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:57:40 -0300

    Ex06 - Soma dos Pares

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,32 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+let somaPares = 0;
+//Usando FOR OF
 
+for (let num of numeros){
+    if (num % 2 == 0){
+        somaPares += num;
+    }
+}
+
+//Utilizando FOR tradicional
+/*
+for (let i = 0; i < numeros.length; i++){
+    if (numeros[i] % 2 == 0){
+        somaPares += numeros[i];
+    }
+}
+*/
+
+//Utilizando WHILE
+/*
+let i = 0;
+while (i < numeros.length){
+    if (numeros[i] % 2 == 0){
+        somaPares += numeros[i];
+    }
+    i++;
+}
+*/
+
+//RESPOSTA FINAL
+console.log(somaPares);
