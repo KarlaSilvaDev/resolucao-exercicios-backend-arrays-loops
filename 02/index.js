543a97be62b27affa77ff6a5eb293f5415ebcd95
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:46:00 -0300

    Adicionadas outras estruturas de repetição

--- 02/index.js
+++ 02/index.js
@@ -2,6 +2,9 @@ const letras = ["A", "a", "B", "C", "E", "e"];
 let soma = 0;
 let encontrada = false;
 
+
+//Utilizando FOR OF
+
 for (let letra of letras){
     if ((letra === "e") || (letra === "E")){
         soma++;
@@ -9,6 +12,31 @@ for (let letra of letras){
     }
 }
 
+
+//Utilizando FOR tradicional
+
+/*
+for (let i = 0; i < letras.length; i++){
+    if ((letras[i] === "e") || (letras[i] == "E")){
+        soma++;
+        encontrada = true;
+    }
+}
+*/
+
+//Utilizando WHILE
+/*
+let i = 0;
+while (i < letras.length){
+    if ((letras[i] === "e") || (letras[i] == "E")){
+        soma++;
+        encontrada = true;
+    }
+    i++;
+}
+*/
+
+//RESPOSTA FINAL
 if (encontrada == true){
     if (soma > 1){
         console.log(`Foram encontradas ${soma} letras "E" ou "e".`);
