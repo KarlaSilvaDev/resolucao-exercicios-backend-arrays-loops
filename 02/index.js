09d87cefae3a62a52ed2dad8a51b9d463d843c2d
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:02:42 -0300

    Ex02 - Contando letras

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,20 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+let soma = 0;
+let encontrada = false;
+
+for (let letra of letras){
+    if ((letra === "e") || (letra === "E")){
+        soma++;
+        encontrada = true;
+    }
+}
+
+if (encontrada == true){
+    if (soma > 1){
+        console.log(`Foram encontradas ${soma} letras "E" ou "e".`);
+    }else{
+        console.log(`Foi encontrada ${soma} letra "E" ou "e". `);
+    }
+}else{
+    console.log('Nenhuma letra "E" ou "e" foi encontrada.');
+}
