35011ce4a8cdc339192d1afbc4ce5f1af2e4b4eb
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 10:59:46 -0300

    Ex14 - Imprima os menores

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,33 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+//Utilizando FOR tradicional
+if (arrayA.length == arrayB.length){
+    for(let i = 0; i < arrayA.length; i++){
+        if (arrayA[i] < arrayB[i]){
+            console.log(arrayA[i]);
+        }else{
+            console.log(arrayB[i]);
+        }
+    }
+}else{
+    console.log("Não foi possível realizar a comparação, pois os Arrays têm tamanhos diferentes!")
+}
+
+
+//Utilizando WHILE
+/*
+if (arrayA.length == arrayB.length){
+    let i = 0;
+    while (i < arrayA.length){
+        if (arrayA[i] < arrayB[i]){
+            console.log(arrayA[i]);
+        }else{
+            console.log(arrayB[i]);
+        }
+        i++;
+    }
+}else{
+    console.log("Não foi possível realizar a comparação, pois os Arrays têm tamanhos diferentes!")
+}
+*/
