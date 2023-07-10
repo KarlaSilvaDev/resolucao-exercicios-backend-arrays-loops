1b61a1c1dc9863ac59cb8f826f4088603a75ddd7
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:10:41 -0300

    Ex03 - Encontre o 10

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,15 @@
 const numeros = [54, 22, 14, 87, 10, 284];
+let encontrado = false;
+let indice = 0;
 
+for (let numero of numeros){
+    if (numero == 10){
+        encontrado = true;
+        console.log(indice);
+    }
+    indice++;
+}
+
+if (encontrado == false){
+    console.log(-1);
+}
