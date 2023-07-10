0e4d9899d848d3066780d4dc4a145c3a95433c21
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 10:34:54 -0300

    Ex11 - Filtrando números

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,34 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 10, 13, 17, 20, 26, 34, 100, 118, 245];
 
+let novo = [] //contem apenas os números do array original que estejam entre 10 e 20, incluindo esses números, ou que sejam maiores que 100
+
+//Utilizando FOR OF
+
+for (let num of original){
+    if (((num>=10)&&(num<=20)) || (num > 100)){
+        novo.push(num);
+    }
+}
+
+
+//Utilizando FOR| tradicional
+/*
+for (let i = 0; i < original.length; i++){
+    if (((original[i]>=10)&&(original[i]<=20)) || (original[i] > 100)){
+        novo.push(original[i]);
+    }
+}
+*/
+
+//Utilizando WHILE
+/*
+let i = 0;
+while (i < original.length){
+    if (((original[i]>=10)&&(original[i]<=20)) || (original[i] > 100)){
+        novo.push(original[i]);
+    }
+    i++;
+}
+*/
+
+console.log(novo)
