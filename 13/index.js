1641662ebbbe292b941959fd0c5dfa5335b780a5
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 10:53:20 -0300

    Ex13 - Verificando Disjuntores

--- 13/index.js
+++ 13/index.js
@@ -1,2 +1,30 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+//Utilizando FOR tradicional
+for (i = 0; i < disjuntores.length; i++){
+    if (disjuntores[i]){
+        console.log(i);
+    }
+}
+
+//Utilizando FOR OF
+/*
+let i = 0;
+for (disjuntor of disjuntores){
+    if (disjuntor){
+        console.log(i);
+    }
+    i++;
+}
+*/
+
+//Utilizando WHILE
+/*
+let i = 0;
+while (i < disjuntores.length){
+    if (disjuntores[i]){
+        console.log(i);
+    }
+    i++;
+}
+*/
