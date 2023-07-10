b5a2f7bdc0967e9cd970a457722eaabca5e50a7f
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-10 09:49:14 -0300

    Adicionadas outras estruturas de repetição

--- 01/index.js
+++ 01/index.js
@@ -1,8 +1,26 @@
 const numeros = [2, 3, 4, 6];
 let soma = 0;
 
+//Utilizando FOR OF
 for (let numero of numeros){
     soma += numero;
 }
 
+//Utilizando FOR tradicional
+/*
+for (let indice = 0; indice < numeros.length; indice++){
+    soma += numeros[indice];
+}
+*/
+
+//Utilizando WHILE
+/*
+let indice = 0;
+while(indice < numeros.length){
+    soma += numeros[indice];
+    indice++;
+}
+*/
+
+//RESPOSTA FINAL
 console.log(soma);
